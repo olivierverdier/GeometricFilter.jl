5169f40fa023da6b341921e30a2691f50cb163fa
Olivier Verdier <olivier.verdier@gmail.com>
2023-09-25 10:38:39 +0200

    clean up test_filter_measure
    it is a proper test
    runs several cases in a for loop

--- test/test_filter_measure.jl
+++ test/test_filter_measure.jl
@@ -21,66 +21,64 @@ rng = Random.default_rng()
 
 function setup_initial_dist(rng, A, dev=1.)
     x0 = rand(rng, group_manifold(A))
-    # D0 = ProjLogNormal(A, x0, cov, DefaultOrthonormalBasis())
-    D0 = ProjLogNormal(A, x0, 1.)
+    D0 = ProjLogNormal(A, x0, dev^2)
     return D0
 end
 
-
-
-# G = MultiDisplacement(2)
-G = SpecialOrthogonal(3)
-A =  GroupOperationAction(G)
-# A = DualGroupOperationAction(G)
-
-M = group_manifold(A)
-
-# G::MultiAffine, direction,
-# OA = MultiAffineAction(G, direction)
-
-function setup_action_observer(rng, OA,  N=10)
+function setup_action_observer(rng, OA, N=10)
     refs = [rand(rng, group_manifold(OA)) for i in 1:N]
     obs = ProductObserver([ActionObserver(OA, ref) for ref in refs]...)
     return obs
 end
 
-# ground truth
-D0 = setup_initial_dist(rng, A)
 
-x0 = D0.μ
-# only valid when M = G:
-# x0 = identity_element(G)
-# update_mean(D0, x0)
+run_filter(D0, N, observer, onoise, measure) =
+    accumulate(1:N; init=D0) do D, i
+        return update(D, Observation(observer, onoise, measure))
+    end
+
+@testset "Observer action $OA\nAction $A\nObserver $(typeof(observer)):" for OA in [
+    GroupOperationAction(SpecialOrthogonal(3)),
+    DualGroupOperationAction(SpecialOrthogonal(3)),
+    MultiAffineAction(MultiDisplacement(2), LeftAction()),
+    MultiAffineAction(MultiDisplacement(2), RightAction()),
+    RotationAction(Euclidean(3), SpecialOrthogonal(3), RightAction()),
+    ], A in [
+        GroupOperationAction(base_group(OA)),
+        DualGroupOperationAction(base_group(OA)),
+    ], observer in
+    [
+        setup_action_observer(rng, OA, 10),
+        IdentityObserver(base_group(A)),
+    ]
+
+    M = group_manifold(A)
+
+    # ground truth
+    D0 = setup_initial_dist(rng, A)
+
+    x0 = D0.μ
 
 
+    m = observer(x0)
 
-# obs = setup_action_observer(rng, MultiAffineAction(G, LeftAction()), 10)
-# obs = setup_action_observer(rng, MultiAffineAction(G, RightAction()), 10)
-V = Euclidean(3)
-# obs = setup_action_observer(rng, RotationAction(V,G, LeftAction()), 10)
-obs = setup_action_observer(rng, RotationAction(V,G, RightAction()), 10)
-# obs = IdentityObserver(G)
-m = obs(x0)
-
-onoise = IsotropicNoise(observation_space(obs), 1.)
+    onoise = IsotropicNoise(observation_space(observer), 1.0)
 
 
-run_filter(D0, N) = accumulate(1:N; init=D0) do D, i
-    return update(D, Observation(obs, onoise, m))
+    # actually run the test
+    noise = ActionNoise(A, 1.0)
+    x1 = noise(rng, x0)
+    D1 = update_mean(D0, x1)
+
+    Ds = run_filter(D1, 2^5, observer, onoise, m)
+
+    # gdist = [distance(group_manifold(A), D0.μ, D.μ) for D in Ds]
+    dists = [distance(group_manifold(A), D0.μ, D.μ) for D in [first(Ds), last(Ds)]]
+
+    improvement_dB = -10*log10(last(dists) / first(dists))
+
+    @test improvement_dB >= 10
 end
-
-
-# actually run the test
-noise = ActionNoise(A, 1.0)
-x1 = noise(rng, x0)
-D1 = update_mean(D0, x1)
-
-# Ds = _run(D1, 1000)
-Ds = run_filter(D1, 3000)
-
-gdist = [distance(M, D0.μ, D.μ) for D in Ds]
-
-last(Ds).μ
 
 # why is the error not exponential as in the localisation test?!?
 
