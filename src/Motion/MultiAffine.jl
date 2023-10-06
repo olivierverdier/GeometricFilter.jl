bd4ae6e6cea4f9351ed8315a6f83bbae0ee1d033
Olivier Verdier <olivier.verdier@gmail.com>
2023-10-06 13:50:46 +0200

    change type order for MultiAffineMotion

--- src/Motion/MultiAffine.jl
+++ src/Motion/MultiAffine.jl
@@ -1,19 +1,19 @@
 
-struct MultiAffineMotion{TA, TG,TM,TAD<:ActionDirection} <: AbstractAffineMotion{TA}
+struct MultiAffineMotion{TAD<:ActionDirection, TA, TG,TM} <: AbstractAffineMotion{TA}
     G::TG # MultiAffine{H, dim, size, 𝔽}
     M::TM # Array{𝔽, 2}; size×size array
 end
 
-Base.show(io::IO, m::MultiAffineMotion{<:Any,<:Any,<:Any, TAD}) where {TAD} = print(io, "MultiAffineMotion($(m.G), $(m.M), $TAD())")
+Base.show(io::IO, m::MultiAffineMotion{TAD}) where {TAD} = print(io, "MultiAffineMotion($(m.G), $(m.M), $TAD())")
 
 @doc raw"""
     MultiAffineMotion(G::DecoratedManifold,M::Array{𝔽, 2},conv::ActionDirection)
 
 One of the affine motions which are neither rigid motions nor translations on the group SE_k(n) (or more general affine groups). If a group element is denoted by \(χ = [X;R]\), then the motion corresponding to the \(k×k\) matrix \(M\) is defined by \(φ(χ) := (XM;0)\).
 """
-MultiAffineMotion(G, M, conv) = MultiAffineMotion{typeof(_get_group_operation_action(G, conv)),typeof(G),typeof(M),typeof(conv)}(G, M)
+MultiAffineMotion(G, M, conv) = MultiAffineMotion{typeof(conv),typeof(_get_group_operation_action(G, conv)),typeof(G),typeof(M)}(G, M)
 
-get_action(m::MultiAffineMotion{<:Any,<:Any,<:Any,TAD}) where {TAD} = _get_group_operation_action(m.G, TAD())
+get_action(m::MultiAffineMotion{TAD}) where {TAD} = _get_group_operation_action(m.G, TAD())
 
 function _lin(m, χ)
     X = submanifold_component(m.G, χ, 1)
@@ -21,8 +21,8 @@ function _lin(m, χ)
     return from_normal_alg(m.G, res)
 end
 
-get_dynamics(m::MultiAffineMotion{<:Any,<:Any,<:Any,LeftAction}, χ)  = _lin(m, χ)
-function get_dynamics(m::MultiAffineMotion{<:Any,<:Any,<:Any,RightAction}, χ) 
+get_dynamics(m::MultiAffineMotion{LeftAction}, χ)  = _lin(m, χ)
+function get_dynamics(m::MultiAffineMotion{RightAction}, χ) 
     R = to_factor_grp(m.G, χ)
     tmp = _lin(m, χ)
     tmp_ = submanifold_component(m.G, tmp, 1)
@@ -34,4 +34,4 @@ end
 
 get_lin(m::MultiAffineMotion) = ξ -> _lin(m, ξ)
 
-swap_group_motion(m::MultiAffineMotion{<:Any,<:Any,<:Any,TAD}) where {TAD} = MultiAffineMotion(m.G, m.M, switch_direction(TAD()))
+swap_group_motion(m::MultiAffineMotion{TAD}) where {TAD} = MultiAffineMotion(m.G, m.M, switch_direction(TAD()))
