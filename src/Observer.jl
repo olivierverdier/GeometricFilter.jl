5c16008e8090ae1aed755b75e5a28e5f8f1939a1
Olivier Verdier <olivier.verdier@gmail.com>
2023-10-12 19:29:49 +0200

    reorganise Observer
    mostly move ProductObserver in separate file

--- src/Observer.jl
+++ src/Observer.jl
@@ -49,41 +49,11 @@ get_tan_observer(obs::AbstractObserver, A, x) = get_tan_observer(obs, A, x, obs(
 
 
 
-struct ProductObserver{TO<:Tuple} <: AbstractObserver
-    observers::TO
-end
+include("Observer/Product.jl")
 
-Base.show(io::IO, obs::ProductObserver) = print(io, "ProductObserver("* join([repr(o) for o in obs.observers], ", ") *")")
-
-"""
-    ProductObserver(observers::AbstractObserver...)
-
-Combine existing observers into a new one.
-
-```julia
-M = ManifoldsBase.DefaulManifold(2)
-# a simple observer for the sake of this example
-ob1 = IdentityObserver(M)
-ob2 = IdentityObserver(M)
-x = rand(M) # a point on M
-ob = ProductObserver(ob1, ob2)
-ob(x) # (x, x)
-```
-"""
-function ProductObserver(observers::AbstractObserver...)
-    return ProductObserver(observers)
-end
-
-observation_space(obs::ProductObserver) = ProductManifold([observation_space(o) for o in obs.observers]...)
-observed_space(obs::ProductObserver) = observed_space(first(obs.observers))
-
-get_measurement(obs::ProductObserver, x) = ArrayPartition([ob(x) for ob in obs.observers]...)
-
-get_tan_observer(obs::ProductObserver, A, x, ys) = ξ -> ArrayPartition([get_tan_observer(ob, A, x, y)(ξ) for (ob,y) in zip(obs.observers, submanifold_components(ys))]...)
-
-include("Observer/Linear.jl")
-include("Observer/Action.jl")
-include("Observer/Identity.jl")
+include("Observer/Simple/Linear.jl")
+include("Observer/Simple/Action.jl")
+include("Observer/Simple/Identity.jl")
 
 
 
---
+++ src/Observer/Product.jl
@@ -0,0 +1,32 @@
+
+struct ProductObserver{TO<:Tuple} <: AbstractObserver
+    observers::TO
+end
+
+Base.show(io::IO, obs::ProductObserver) = print(io, "ProductObserver("* join([repr(o) for o in obs.observers], ", ") *")")
+
+"""
+    ProductObserver(observers::AbstractObserver...)
+
+Combine existing observers into a new one.
+
+```julia
+M = ManifoldsBase.DefaulManifold(2)
+# a simple observer for the sake of this example
+ob1 = IdentityObserver(M)
+ob2 = IdentityObserver(M)
+x = rand(M) # a point on M
+ob = ProductObserver(ob1, ob2)
+ob(x) # (x, x)
+```
+"""
+function ProductObserver(observers::AbstractObserver...)
+    return ProductObserver(observers)
+end
+
+observation_space(obs::ProductObserver) = ProductManifold([observation_space(o) for o in obs.observers]...)
+observed_space(obs::ProductObserver) = observed_space(first(obs.observers))
+
+get_measurement(obs::ProductObserver, x) = ArrayPartition([ob(x) for ob in obs.observers]...)
+
+get_tan_observer(obs::ProductObserver, A, x, ys) = ξ -> ArrayPartition([get_tan_observer(ob, A, x, y)(ξ) for (ob,y) in zip(obs.observers, submanifold_components(ys))]...)
