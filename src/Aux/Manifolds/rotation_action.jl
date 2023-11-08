71005f8c8017af78c82cbf5ac68c619f7eaae609
Olivier Verdier <olivier.verdier@gmail.com>
2023-11-08 15:12:56 +0100

    minor streamlining

--- src/Aux/Manifolds/rotation_action.jl
+++ src/Aux/Manifolds/rotation_action.jl
@@ -1,30 +1,25 @@
 
 
-function Manifolds.apply_diff_group(
+Manifolds.apply_diff_group(
     A::Manifolds.RotationActionOnVector{RightAction},
     Id::Identity,
     X,
     p,
-) 
-    return -apply_diff_group(switch_direction(A), Id, X, p)
-end
+) = -apply_diff_group(switch_direction(A), Id, X, p)
 
-function Manifolds.apply_diff_group(
+Manifolds.apply_diff_group(
     ::Manifolds.ColumnwiseMultiplicationAction{LeftAction},
     ::Identity,
     X,
     p,
-)
-    return X * p
-end
+) = X * p
 
 Manifolds.apply!(::RotationAction{LeftAction}, q, a, p) = LinearAlgebra.mul!(q, a, p)
 
 Manifolds.apply!(A::Manifolds.RotationAction{RightAction}, q, a, p) = Manifolds.apply!(switch_direction(A), q, inv(base_group(A), a), p)
 
 
-function Manifolds.apply_diff(A::Manifolds.ColumnwiseMultiplicationAction{LeftAction}, a, ::Any, X)
-    return apply(A, a, X)
+Manifolds.apply_diff(A::Manifolds.ColumnwiseMultiplicationAction{LeftAction}, a, ::Any, X) = apply(A, a, X)
 end
 
 
