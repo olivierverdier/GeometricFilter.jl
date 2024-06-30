79a84ecc7580ee63461716ea58e144b65904e50b
Olivier Verdier <olivier.verdier@gmail.com>
2024-06-30 21:36:13 +0200

    test_integ: cleanup

--- test/motion/test_integ.jl
+++ test/motion/test_integ.jl
@@ -7,51 +7,62 @@ import Random
 
 rng = Random.default_rng()
 
-@testset "Motion" for G in [SpecialOrthogonal(4),
-                            # SpecialEuclidean(4),
+@testset "Motion $G" for G in [SpecialOrthogonal(4),
+                            SpecialEuclidean(4),
+                            MultiDisplacement(4),
                             MultiDisplacement(4,2),
                             ]
-    # G = SpecialOrthogonal(4)
-    # G = MultiDisplacement(4,2)
-    # G = SpecialEuclidean(4)
     action = GroupOperationAction(G)
     vel = rand(rng, GroupTools.algebra(G))
     # submanifold_component(vel, 1) .= 0
     # rm = make_rigid_motion(action, vel)
     rm = RigidMotion(action, vel)
-    @test_throws ErrorException RigidMotion(action, 0)
-    rm0 = rm + ZeroMotion(GroupOperationAction(G))
-    @test rm0 isa RigidMotion
-    @test rm0 ≈ rm
 
-    @test_throws TypeError RigidMotion(GroupOperationAction(G,(RightAction(), RightSide())), vel)
-    # @show rm'(identity_element(G))
+    @testset "ZeroMotion" begin
+        rm0 = rm + ZeroMotion(GroupOperationAction(G))
+        @test rm0 isa RigidMotion
+        @test rm0 ≈ rm
+    end
+
+    @testset "RigidMotion Exceptions" begin
+        @test_throws ErrorException RigidMotion(action, 0)
+        @test_throws TypeError RigidMotion(GroupOperationAction(G,(RightAction(), RightSide())), vel)
+        # @show rm'(identity_element(G))
+    end
+
     sol = GeometricFilter.integrate_lift(1.0*rm, identity_element(G), .01)
     # test that sol(1) ≡ exp(ξ), for a rigid motion ξ
-    χ = sol(1)
     expected = exp_lie(G, vel)
-    @test isapprox(G, χ, expected)
 
-    rm_ = RigidMotion(action, -vel)
-    sol_ = GeometricFilter.integrate_lift(rm+rm_, identity_element(G), .01)
-    id = sol_(1)
-    @test isapprox(G, id, identity_element(G))
+    @testset "Integration" begin
+        computed = last(sol)
+        # @error G vel computed expected
+        @test isapprox(G, computed, expected)
 
-    tm = TranslationMotion(G, vel, LeftAction())
-    sol = GeometricFilter.integrate_lift(tm, identity_element(G), .1)
+        rm_ = RigidMotion(action, -vel)
+        sol_ = GeometricFilter.integrate_lift(rm+rm_, identity_element(G), .01)
+        id = last(sol_)
+        @test isapprox(G, id, identity_element(G))
+
+        tm = TranslationMotion(G, vel, LeftAction())
+        sol = GeometricFilter.integrate_lift(tm, identity_element(G), .1)
+        # TODO: missing test here
+    end
 
     # vel_ = rand(rng, GeometricFilter.algebra(G))
     # v3 = lie_bracket(G, vel, vel_)
     # @show vel_
     # @show adjoint_action(G, inv(G, χ), v3)
 
-    B = DefaultOrthogonalBasis()
-    # mm = get_adjoint_matrix(G, vel, B)
-    mm = GeometricFilter.get_lin_mat(rm, identity_element(G), B)
-    res_mat = GeometricFilter.compose_adjoint(G, inv(G, χ), exp(mm), B)
+    @testset "compose adjoint" begin
+        B = DefaultOrthogonalBasis()
+        # mm = get_adjoint_matrix(G, vel, B)
+        mm = GeometricFilter.get_lin_mat(rm, identity_element(G), B)
+        res_mat = GeometricFilter.compose_adjoint(G, inv(G, expected), exp(mm), B)
+        # display(res_mat)
+        @test isapprox(res_mat, LinearAlgebra.I)
+    end
 
 
-    # display(res_mat)
-    @test isapprox(res_mat, LinearAlgebra.I)
 
 end
